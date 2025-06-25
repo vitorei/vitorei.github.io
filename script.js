2c49a95cbd36f2493694e5194b5c71894cd0d8d8
vitorei <00001128157627sp@al.educacao.sp.gov.br>
2025-06-25 16:10:16 -0300

    buoo

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// Definindo as variáveis
 let slideIndex = 0;
 
-// Função para mover o slide
 function moveSlide(n) {
     const slides = document.querySelectorAll('.carousel-item');
     slideIndex += n;
@@ -15,15 +13,12 @@ function moveSlide(n) {
     updateCarousel(slides);
 }
 
-// Função para atualizar o carrossel
 function updateCarousel(slides) {
-    // Reseta todos os slides
     slides.forEach(slide => {
         slide.style.transform = `translateX(-${slideIndex * 100}%)`;
     });
 }
 
-// Inicialização automática do carrossel
 setInterval(() => {
-    moveSlide(1);  // Move automaticamente a cada 3 segundos
-}, 3000);
+    moveSlide(1);
+}, 4000);
