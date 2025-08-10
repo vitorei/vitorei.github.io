e9523e5d32dc5fae92c300e4d6603b31234aad94
vitorei <00001128157627sp@al.educacao.sp.gov.br>
2025-08-10 00:08:18 -0300

    ftjwe

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ let currentSlide = 0;
 
 function showSlide(index) {
     const slides = document.querySelectorAll('.carousel-item');
-    if (index >= slides.length) currentSlide = 0;
-    else if (index < 0) currentSlide = slides.length - 1;
+    const totalSlides = slides.length;
+    const carousel = document.querySelector('.carousel');
+
+    if (index >= totalSlides) currentSlide = 0;
+    else if (index < 0) currentSlide = totalSlides - 1;
     else currentSlide = index;
 
-    slides.forEach((slide, i) => {
-        slide.style.display = i === currentSlide ? 'block' : 'none';
-    });
+    const offset = -currentSlide * 100;
+    carousel.style.transform = `translateX(${offset}%)`;
 }
 
 function moveSlide(step) {
